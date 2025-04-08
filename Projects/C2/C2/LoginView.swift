39c7276b5e8b5d46dba0d778b3a85726c95bd65f
HanHyeBeen <vvv5974033@gmail.com>
2025-04-08 10:48:16 +0900

    Initial commit

---
+++ Projects/C2/C2/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  C2
+//
+//  Created by Enoch on 4/15/25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}

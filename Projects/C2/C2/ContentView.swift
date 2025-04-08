e384b69e6fc88a8d4d321d6a1224d6361feb2170
HanHyeBeen <vvv5974033@gmail.com>
2025-04-08 10:48:16 +0900

    Initial commit

---
+++ Projects/C2/C2/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  C2
+//
+//  Created by Enoch on 4/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Challeng 2")
+            Text("개인프로젝트")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

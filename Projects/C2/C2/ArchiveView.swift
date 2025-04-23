17af634e160d7fc9c42127dc47d3d769566811f8
HanHyeBeen <vvv5974033@gmail.com>
2025-04-23 16:31:41 +0900

    Mentor Mode ArchiveView Fix

--- Projects/C2/C2/ArchiveView.swift
+++ Projects/C2/C2/ArchiveView.swift
@@ -118,42 +118,44 @@ struct ArchiveView: View {
         let questions = learnerDict[learner] ?? []
         let hasUnanswered = questions.contains { $0.memo == nil }
         
-        NavigationLink {
-            DetailView(mentor: nil, learner: learner, questions: learnerDict[learner] ?? [], itemTitle: learner.name, itemSub: learner.field)
-        } label: {
-            ZStack {
-                Group {
-                    if learner.field == "Tech" {
-                        Image("Tech")
-                            .resizable()
-                    } else if learner.field == "Design" {
-                        Image("Design")
-                            .resizable()
-                    } else if learner.field == "Domain" {
-                        Image("Domain")
-                            .resizable()
-                    } else {
-                        Image("Etc")
-                            .resizable()
+        ZStack(alignment: .topTrailing) {
+            NavigationLink {
+                DetailView(mentor: nil, learner: learner, questions: learnerDict[learner] ?? [], itemTitle: learner.name, itemSub: learner.field)
+            } label: {
+                ZStack {
+                    Group {
+                        if learner.field == "Tech" {
+                            Image("Tech")
+                                .resizable()
+                        } else if learner.field == "Design" {
+                            Image("Design")
+                                .resizable()
+                        } else if learner.field == "Domain" {
+                            Image("Domain")
+                                .resizable()
+                        } else {
+                            Image("Etc")
+                                .resizable()
+                        }
                     }
+                    .frame(width: 100, height: 100)
+                    .cornerRadius(8)
+                    
+                    Text(learner.name)
+                        .font(Font.custom("UhBee Se_hyun Bold", size: 14))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(C2App.TextPrimary)
+                        .padding(.bottom, 20)
                 }
-                .frame(width: 100, height: 100)
-                .cornerRadius(8)
-                
-                Text(learner.name)
-                    .font(Font.custom("UhBee Se_hyun Bold", size: 14))
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(C2App.TextPrimary)
-                    .padding(.bottom, 20)
             }
-        }
-        .buttonStyle(PlainButtonStyle())
-        
-        if hasUnanswered {
-            Circle()
-                .fill(Color.red)
-                .frame(width: 12, height: 12)
-                .offset(x: 6, y: -6)
+            .buttonStyle(PlainButtonStyle())
+            
+            if hasUnanswered {
+                Image("NewIcon")
+                    .resizable()
+                    .frame(width: 21, height: 21)
+                    .offset(x: -3, y: 7)
+            }
         }
     }
 }
